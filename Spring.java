af49bdaf3e03b9143ffe4957cb32330da1f5e572
tumasyanerik58 <31702127+tumasyanerik58@users.noreply.github.com>
2022-05-18 03:48:21 +0400

    t1 t2

--- Spring.java
+++ Spring.java
@@ -1,8 +1,25 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Spring {
 
     private double k;
+
+    public Spring(){
+        this.k = 1;
+    }
+
+    public Spring(double number){
+        this.k = number;
+    }
+
+    private void setStiffness(double number){
+        this.k = number;
+    }
+
+    public double getStiffness(){
+        return this.k;
+    }
 
     private ArrayList<Double> coordinates(int t1, int t2, int dt, int x, int v, int m){
         ArrayList<Double> coordinates = null;
@@ -14,20 +31,14 @@ public class Spring {
         return coordinates;
     }
 
-    public Spring(){
-        this.k = 1;
+    public Spring inParallel(Spring that){
+        double newK = this.k + that.getStiffness();
+        return new Spring(newK);
     }
 
-    public Spring(int number){
-        this.k = number;
-    }
-
-    private void setStiffness(int number){
-        this.k = number;
-    }
-
-    public double getStiffness(){
-        return this.k;
+    public Spring inSeries(Spring that){
+        double newK = 1 / (1/this.k + 1/that.getStiffness());
+        return new Spring(newK);
     }
 
     public ArrayList<Double> firstMove(int t, int dt, int x, int v){
@@ -44,5 +55,89 @@ public class Spring {
 
     public ArrayList<Double> fourthMove(int t1,int t2, int dt, int x, int v, int m){
         return this.coordinates(t1,t2,dt,x,v,m);
+    }
+
+    class SpringArray{
+        public static Double getInSeries(int index, String expression,ArrayList<Double> stiffnessArr) {
+            Double defaultK = Double.valueOf(0);
+            int i = index + 1;
+            while (true){
+                if(String.valueOf(expression.charAt(i)) == "]" || String.valueOf(expression.charAt(i)) == "}"){
+                    i++;
+                    break;
+                } else if(String.valueOf(expression.charAt(i)) == "["){
+                    if(String.valueOf(expression.charAt(i+1)) == "]"){
+                        Double firstStiffness = stiffnessArr.get(0);
+                        stiffnessArr.remove(0);
+                        if(firstStiffness != null){
+                            defaultK = defaultK + 1/firstStiffness;
+                        } else {
+                            defaultK = defaultK + 1;
+                        }
+                        Double newK = getInParallel(i,expression,stiffnessArr);
+                        if(newK != null){
+                            defaultK = defaultK + 1/newK;
+                        }
+                    }
+                }  else if(String.valueOf(expression.charAt(i+1)) == "{"){
+                    Double newK = getInSeries(i, expression, stiffnessArr);
+                    if(newK != null){
+                        defaultK = defaultK + 1/newK;
+                    }
+                }
+            }
+            return 1/defaultK;
+        }
+
+        public static Double getInParallel(int index, String expression,ArrayList<Double> stiffnessArr) {
+            Double defaultK = Double.valueOf(0);
+            int i = index + 1;
+            while (true){
+                if(String.valueOf(expression.charAt(i)) == "]" || String.valueOf(expression.charAt(i)) == "}"){
+                    i++;
+                    break;
+                } else if(String.valueOf(expression.charAt(i)) == "["){
+                    if(String.valueOf(expression.charAt(i+1)) == "]"){
+                        Double firstStiffness = stiffnessArr.get(0);
+                        stiffnessArr.remove(0);
+                        if(firstStiffness != null){
+                            defaultK = defaultK + firstStiffness;
+                        } else {
+                            defaultK = defaultK + 1;
+                        }
+                        Double newK = getInParallel(i,expression,stiffnessArr);
+                        defaultK = defaultK + newK;
+                    }
+                }  else if(String.valueOf(expression.charAt(i+1)) == "{"){
+                    Double newK = getInSeries(i, expression, stiffnessArr);
+                    defaultK = defaultK + newK;
+                }
+            }
+            return defaultK;
+        }
+
+        public static Spring equivalentSpring(String springExpr){
+            Double k = null;
+            if(String.valueOf(springExpr.charAt(0)) == "["){
+                k = getInParallel(0 ,springExpr, null);
+            } else {
+                k = getInSeries(0, springExpr, null);
+            }
+            return new Spring(k);
+        }
+
+        public static Spring equivalentSpringWithSpring(String springExpr, ArrayList<Spring> springs){
+            Double k = null;
+            ArrayList<Double> stiffnessArr = null;
+            for(Spring spring: springs){
+                stiffnessArr.add(spring.getStiffness());
+            }
+            if(String.valueOf(springExpr.charAt(0)) == "["){
+                k = getInParallel(0 ,springExpr, stiffnessArr);
+            } else {
+                k = getInSeries(0, springExpr, stiffnessArr);
+            }
+            return new Spring(k);
+        }
     }
 }
